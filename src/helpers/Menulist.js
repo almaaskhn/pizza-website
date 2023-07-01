78a3e79bde6bc1d3c3027c08a577a17db3c644ee
Almaas <almaaskn@gmail.com>
2023-07-01 16:42:51 +0530

    first commit

---
+++ src/helpers/Menulist.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import AlmaasSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 199,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 119,
+  },
+  {
+    name: "Four Cheese Pizza",
+    image: AlmaasSpecial,
+    price: 299,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 170,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 150,
+  },
+  {
+    name: "Flavor Fusion",
+    image: Expensive,
+    price: 1999,
+  },
+];
